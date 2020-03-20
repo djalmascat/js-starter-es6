649b23ef00892d9efb46758d12ac66ca03620900
de Araujo Lessa <AllanBruno.Lessa@cencosud.com.br>
2020-03-20 00:08:05 -0300

    Valores padrão em arrow functions

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
-const arr = [1, 3, 4, 5, 6];
 
-const newArr = arr.map(item => item * 2);
 
-console.log(newArr);
+const soma = (a = 3, b = 6) => a + b;
 
-const teste = () => ({ nome: 'Bruno Lessa'});
-
-console.log(teste());
+console.log(soma(1));
+console.log(soma());
