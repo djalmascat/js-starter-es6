9b70117981258c06238019f04156b0d14c99af6b
Allan Lessa <djalmascat@gmail.com>
2020-03-25 11:40:57 -0300

    Template Literals

--- main.js
+++ main.js
@@ -1,52 +1,5 @@
-//REST
-//Ex 1
-const usuario = {
-    nome: "Bruno",
-    idade: 36,
-    empresa: "Cencosud"
-};
+const nome = "Bruno";
+const idade = 36;
 
-const {
-    nome,
-    ...resto
-} = usuario
-
-console.log(nome);
-console.log(resto);
-
-//Ex 2
-const arr = [1, 2, 3, 4];
-
-const [a, b, ...c] = arr
-console.log(a);
-console.log(b);
-console.log(c);
-
-//Ex 3
-function soma(a, b, ...params) {
-    //return params.reduce((total, next) => total + next);
-    return params;;
-}
-console.log(soma(1, 3, 4, 4, 5, 6, 7));
-
-//SPREAD
-
-//Ex 1
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = [...arr1, ...arr2];
-console.log(arr3);
-
-//Ex 2
-
-const usuario1 = {
-    nome: 'Bruno',
-    idade: 36,
-    empresa: 'Cencosud',
-};
-
-const usuario2 = {
-    ...usuario1,
-    nome: 'Allan'
-};
-console.log(usuario2);
+//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
