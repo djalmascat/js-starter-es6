fce98b378a7e080f4d7122d64cf05927376b7e0d
Allan Lessa <djalmascat@gmail.com>
2020-03-26 17:59:23 -0300

    Mod01- Ex01

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-const nome = "Bruno";
-const idade = 36;
+class Usuario {
+    constructor(email, senha) {
+        this.email = email;
+        this.senha = senha;
 
-//console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
-console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
+    }
+    isAdmin() {
+        if (this.admin === true) {
+            return this.admin;
+        } else {
+            this.admin = false;
+            return this.admin;
+        }
+    }
+}
+
+class Admin extends Usuario {
+    constructor(email, senha) {
+        super(email, senha);
+        this.admin = true;
+    }
+}
+
+const Adm1 = new Admin('djalmascat@gmail.com', '123');
+console.log(Adm1);
+console.log(Adm1.isAdmin()) // true
+
+const User1 = new Usuario('lanebrankinha@gmail.com', '456');
+console.log(User1);
+console.log(User1.isAdmin()) // false
